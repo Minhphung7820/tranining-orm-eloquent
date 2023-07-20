4e4aae8411059e5bb78742f49da4d2c94cc4fe8c
Trương Minh Phung <minhphung485@gmail.com>
2023-07-20 22:18:11 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -16,36 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // $instance = User::query();
-    // $users = $instance->paginate(50);
-    // $usersCountByStatus = User::query()->select([
-    //     'users.status',
-    //     DB::raw('CASE 
-    //                 WHEN status = 1 THEN "admin"
-    //                 WHEN status = 2 THEN "user"
-    //                 WHEN status = 3 THEN "customer"
-    //                 WHEN status = 4 THEN "customer"
-    //                 WHEN status = 5 THEN "customer"
-    //                 WHEN status = 6 THEN "customer"
-    //                 WHEN status = 7 THEN "customer"
-    //                 WHEN status = 8 THEN "customer"
-    //                 WHEN status = 9 THEN "customer"
-    //                 WHEN status = 10 THEN "customer"
-    //                 WHEN status = 11 THEN "customer"
-    //                 WHEN status = 12 THEN "customer"
-    //                 WHEN status = 13 THEN "customer"
-    //                 WHEN status = 14 THEN "customer"
-    //                 WHEN status = 15 THEN "customer"
-    //             END as status_text'),
-    //     DB::raw('COUNT(*) as total')
-    // ])
-    //     ->groupBy('status')
-    //     ->get();
-
-    // $data = array_merge([
-    //     'data' => collect($users)->toArray()
-    // ], ['additional_data' => collect($usersCountByStatus)->toArray()]);
-
 
 
     $statusLabels = [
