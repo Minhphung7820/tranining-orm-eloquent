676e173145697412e4e9671ce4870dd357a14c3f
Trương Minh Phung <minhphung485@gmail.com>
2023-08-13 11:58:37 +0700

    up

--- app/Http/Controllers/API/TimekeepingOvertime.php
+++ app/Http/Controllers/API/TimekeepingOvertime.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Constants\CommonConstants;
 use App\Http\Controllers\Controller;
+use App\Models\Employee;
 use App\Models\Timekeeping;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TimekeepingOvertime extends Controller
 {
@@ -62,6 +65,12 @@ class TimekeepingOvertime extends Controller
             });
         }
 
+        if (isset($request->keyword) && $request->keyword) {
+            $timeKeepings->whereHas('employee', function ($query) use ($request) {
+                $query->whereRaw('LOWER(name) LIKE ?', ['%' . strtolower($request->keyword) . '%']);
+            });
+        }
+
         if (isset($request->type_overtime) && $request->type_overtime) {
             switch ($request->type_overtime) {
                 case 'overtime_with_coefficient':
@@ -84,10 +93,65 @@ class TimekeepingOvertime extends Controller
             }
         }
 
-        $data = $timeKeepings->paginate($request->limit);;
+        $data = collect($timeKeepings->paginate($request->limit))->toArray();
+        $total_by_form_shift = [];
+        foreach (CommonConstants::TYPE_OVERTIME as $label => $value) {
+            $total_by_form_shift[] = [
+                'type_name' => $label,
+                'total_count' => $this->getCountEmployeeByTypeOvertime($label, $request)
+            ];
+        }
+        $data['total_by_form_shift'] = $total_by_form_shift;
         return response()->json($data);
     }
 
+
+    public function getCountEmployeeByTypeOvertime($type, $request)
+    {
+        $employee = Employee::with([
+            'shift' => function ($query) use ($type, $request) {
+                $query->whereHas('configOvertimes', function ($subQuery) use ($type, $request) {
+                    switch ($type) {
+                        case 'overtime_with_coefficient':
+                            $subQuery->where('form_shift', 'calculate_salary')
+                                ->where('coefficient', '>', 1);
+                            break;
+                        case 'uncompensated_overtime':
+                            $subQuery->where('form_shift', 'calculate_salary')
+                                ->where('coefficient', '=', 1);
+                            break;
+                        case 'compensatory_time_off':
+                            $subQuery->where('form_shift', 'off_compensate');
+                            break;
+                    }
+                });
+            }
+        ]);
+
+        if (isset($request->department_id) && $request->department_id) {
+            $employee->whereHas('department', function ($query) use ($request) {
+                $query->where('departments.id', $request->department_id);
+            });
+        }
+
+        if (isset($request->position_id) && $request->position_id) {
+            $employee->whereHas('position', function ($query) use ($request) {
+                $query->where('positions.id', $request->position_id);
+            });
+        }
+
+        if (isset($request->job_title_id) && $request->job_title_id) {
+            $employee->whereHas('job_title', function ($query) use ($request) {
+                $query->where('job_titles.id', $request->job_title_id);
+            });
+        }
+
+        if (isset($request->keyword) && $request->keyword) {
+            $employee->whereRaw('LOWER(name) LIKE ?', ['%' . strtolower($request->keyword) . '%']);
+        }
+        $count = $employee->count();
+        return $count;
+    }
     /**
      * Show the form for creating a new resource.
      *
