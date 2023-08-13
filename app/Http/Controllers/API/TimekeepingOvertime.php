41a11e929bc3cfa82250732ca9324436f29a9254
Trương Minh Phung <minhphung485@gmail.com>
2023-08-13 09:59:40 +0700

    up

--- app/Http/Controllers/API/TimekeepingOvertime.php
+++ app/Http/Controllers/API/TimekeepingOvertime.php
@@ -15,7 +15,76 @@ class TimekeepingOvertime extends Controller
      */
     public function index(Request $request)
     {
-        $data = Timekeeping::getSummaryOvertimeTimekeeping($request);
+        $year = (int)now()->year;
+        $month = (int)now()->month;
+
+        if (isset($request->year) && $request->year) {
+            $year = (int) $request->year;
+        }
+
+        if (isset($request->month) && $request->month) {
+            $month = (int) $request->month;
+        }
+
+        $timeKeepings = Timekeeping::withYearAndMonth($year, $month)
+            ->with([
+                'employee:id,name,shift_id,position_id,department_id,job_id',
+                'employee.department:id,name',
+                'employee.position:id,name',
+                'employee.job_title:id,name',
+                'shift:id,name_shift',
+                'shift.configOvertimes:id,shift_id,form_shift,coefficient'
+            ])->select([
+                'employee_id',
+                'shift_id',
+                'type',
+            ])->groupBy([
+                'employee_id',
+                'shift_id',
+                'type',
+            ])->where('type', 'overtime');
+
+        if (isset($request->department_id) && $request->department_id) {
+            $timeKeepings->whereHas('employee.department', function ($query) use ($request) {
+                $query->where('departments.id', $request->department_id);
+            });
+        }
+
+        if (isset($request->position_id) && $request->position_id) {
+            $timeKeepings->whereHas('employee.position', function ($query) use ($request) {
+                $query->where('positions.id', $request->position_id);
+            });
+        }
+
+        if (isset($request->job_title_id) && $request->job_title_id) {
+            $timeKeepings->whereHas('employee.job_title', function ($query) use ($request) {
+                $query->where('job_titles.id', $request->job_title_id);
+            });
+        }
+
+        if (isset($request->type_overtime) && $request->type_overtime) {
+            switch ($request->type_overtime) {
+                case 'overtime_with_coefficient':
+                    $timeKeepings->whereHas('shift.configOvertimes', function ($query) {
+                        $query->where('overtime_configs.form_shift', 'calculate_salary');
+                        $query->where('coefficient', '>', 1);
+                    });
+                    break;
+                case 'uncompensated_overtime':
+                    $timeKeepings->whereHas('shift.configOvertimes', function ($query) {
+                        $query->where('overtime_configs.form_shift', 'calculate_salary');
+                        $query->where('coefficient', 1);
+                    });
+                    break;
+                case 'compensatory_time_off':
+                    $timeKeepings->whereHas('shift.configOvertimes', function ($query) {
+                        $query->where('overtime_configs.form_shift', 'off_compensate');
+                    });
+                    break;
+            }
+        }
+
+        $data = $timeKeepings->paginate($request->limit);;
         return response()->json($data);
     }
 
--- app/Models/Timekeeping.php
+++ app/Models/Timekeeping.php
@@ -35,82 +35,11 @@ class Timekeeping extends Model
         return $this->belongsTo(EmployeeShift::class, 'shift_id');
     }
 
-    public static function getSummaryOvertimeTimekeeping($request)
+    public static function withYearAndMonth($year, $month)
     {
-
-        if (isset($request->year) && $request->year) {
-            static::$timefillter['year']  = (int)$request->year;
-        } else {
-            static::$timefillter['year']  =  (int)now()->year;
-        }
-
-        if (isset($request->month) && $request->month) {
-            static::$timefillter['month']  = (int)$request->month;
-        } else {
-            static::$timefillter['month']  =  (int)now()->month;
-        }
-
-        $timeKeepings = self::query()
-            ->with([
-                'employee:id,name,shift_id,position_id,department_id,job_id',
-                'employee.department:id,name',
-                'employee.position:id,name',
-                'employee.job_title:id,name',
-                'shift:id,name_shift',
-                'shift.configOvertimes:id,shift_id,form_shift,coefficient'
-            ])->select([
-                'employee_id',
-                'shift_id',
-                'type',
-            ])->groupBy([
-                'employee_id',
-                'shift_id',
-                'type',
-            ])->where('type', 'overtime');
-
-        if (isset($request->department_id) && $request->department_id) {
-            $timeKeepings->whereHas('employee.department', function ($query) use ($request) {
-                $query->where('departments.id', $request->department_id);
-            });
-        }
-
-        if (isset($request->position_id) && $request->position_id) {
-            $timeKeepings->whereHas('employee.position', function ($query) use ($request) {
-                $query->where('positions.id', $request->position_id);
-            });
-        }
-
-        if (isset($request->job_title_id) && $request->job_title_id) {
-            $timeKeepings->whereHas('employee.job_title', function ($query) use ($request) {
-                $query->where('job_titles.id', $request->job_title_id);
-            });
-        }
-
-        if (isset($request->type_overtime) && $request->type_overtime) {
-            switch ($request->type_overtime) {
-                case 'overtime_with_coefficient':
-                    $timeKeepings->whereHas('shift.configOvertimes', function ($query) {
-                        $query->where('overtime_configs.form_shift', 'calculate_salary');
-                        $query->where('coefficient', '>', 1);
-                    });
-                    break;
-                case 'uncompensated_overtime':
-                    $timeKeepings->whereHas('shift.configOvertimes', function ($query) {
-                        $query->where('overtime_configs.form_shift', 'calculate_salary');
-                        $query->where('coefficient', 1);
-                    });
-                    break;
-                case 'compensatory_time_off':
-                    $timeKeepings->whereHas('shift.configOvertimes', function ($query) {
-                        $query->where('overtime_configs.form_shift', 'off_compensate');
-                    });
-                    break;
-            }
-        }
-
-        $data = $timeKeepings->paginate($request->limit);
-
-        return $data;
+        static::$timefillter['year']  = $year;
+        static::$timefillter['month']  = $month;
+        return new static;
     }
 
     public function getDataOvertimeTimekeepingAttribute()
